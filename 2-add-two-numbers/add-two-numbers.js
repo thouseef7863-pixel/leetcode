1cf6b2eb5d36a565478e2c36d70ca0cf054b8f03
MAHAMMAD THOUSEEF <thouseef7863@gmail.com>
2026-09-14 08:59:39 +0530

    Time: 3 ms (54.62%) | Memory: 59.3 MB (99.18%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+
+    let dummy = new ListNode(0);
+    let current = dummy;
+
+    let carry = 0;
+
+    while (l1 !== null || l2 !== null || carry !== 0) {
+
+        let x = l1 !== null ? l1.val : 0;
+        let y = l2 !== null ? l2.val : 0;
+
+        let sum = x + y + carry;
+
+        let digit = sum % 10;
+
+        carry = Math.floor(sum / 10);
+
+        current.next = new ListNode(digit);
+        current = current.next;
+
+        if (l1 !== null) {
+            l1 = l1.next;
+        }
+
+        if (l2 !== null) {
+            l2 = l2.next;
+        }
+    }
+
+    return dummy.next;
+}
+    
